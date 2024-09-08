f8a62d5ba534306ba678209ef203809dfe757a8b
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-08 16:52:19 +0530

    db error

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from sqlalchemy import create_engine
+
 db=SQLAlchemy()
 DB_NAME="database.db"
 
@@ -13,5 +16,15 @@ def create_app():
     from .auth import auth
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+
+    from .models import User, Book
+
+    create_database(app)
+
     return app
+
+def create_database(application):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app=application)
+        print('database created')
     
--- website/models.py
+++ website/models.py
@@ -6,9 +6,11 @@ class Book(db.Model):
     id = db.Column(db.Integer,primary_key=True)#auto incremented
     book_name = db.Column(db.String(200))
     author_name = db.Column(db.String(100))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 class User(db.Model,UserMixin): #login with usermixin
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(100),unique=True)
     passsword = db.Column(db.String(100))
     first_name = db.Column(db.String(100))
+    Book = db.relationship('Book')
