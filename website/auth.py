ed7268f8b5d0bbea8a28ba5ae4af00f78a413fea
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-08 14:24:38 +0530

    added conditions for singing in, added flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,30 @@
-from flask import Blueprint,render_template,request 
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    data=request.form
-    print(data )
     return render_template("login.html",boolean=True)
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 @auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+
+        if len(email)<4:
+            flash('email must be greater than 4 charaters',category='error')
+        elif len(firstName)<2:
+            flash('Firstname must be greater than 2 charaters',category='error')
+        elif password1 != password2:
+            flash('passwords dosent match',category='error')
+        elif len(password1)<7:
+            flash('password must be greater than 7 charaters',category='error')
+        else:
+            flash('account created',category='success')
+            
     return render_template("signup.html")
