2e1452f7e019baf799351cd6adfd80d8c6c4b0b8
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-08 16:13:27 +0530

    defined the columns of table in database

---
+++ website/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin  #custom class 
+from sqlalchemy.sql import func
+
+class Book(db.Model):
+    id = db.Column(db.Integer,primary_key=True)#auto incremented
+    book_name = db.Column(db.String(200))
+    author_name = db.Column(db.String(100))
+
+class User(db.Model,UserMixin): #login with usermixin
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(100),unique=True)
+    passsword = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
